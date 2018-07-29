a5f17177d0247376de1b40251c7d5c16349f3290
Vladimir56r <opora201756@mail.ru>
2018-07-29 14:34:14 +0300

    Add tests

--- control0.ctl
+++ control0.ctl
@@ -1,7 +1,24 @@
 {
-"command" : "processFiles",
-"papers" : "select id from papers",
+"command" : "getPapersByKeyWords",
+"query" : "Accurate mobility \"location prediction\"", 
+"patents" : false, 
+"citations" : false,
+"start_paper" : 1,
 
+"google_max_papers" : 10,
+
+"google_get_files" : false,
+"google_download_again" : true,
+"google_cluster_files" : true,
+
+"sci_hub_files" : true,
+"sci_hub_title_search" : true,
+"sci_hub_show_captcha" : false,
+"sci_hub_download_captcha" : true,
+"sci_hub_timeout" : 3,
+"sci_hub_capcha_autosolve" : 10,
+
+"commit_iterations" : 1,
 "http_contiguous_requests" : 20,
 "limit_resp_for_one_code" : 1
 }
--- test.bat
+++ test.bat
@@ -1,5 +1,6 @@
-python bibliometer.py -l logbook.log -d first_db_.db3 -c control3.ctl -p proxies.txt
-python bibliometer.py -l logbook.log -d first_db_.db3 -c control4.ctl -p proxies.txt
-python bibliometer.py -l logbook.log -d first_db_.db3 -c control2.ctl -p proxies.txt
-python bibliometer.py -l logbook.log -d first_db_.db3 -c control5.ctl -p proxies.txt
-python bibliometer.py -l logbook.log -d first_db_.db3 -c control6.ctl -p proxies.txt
+python bibliometer.py -l logbook_test.log -d test.db3 -c control0.ctl -p proxies.txt
+python bibliometer.py -l logbook_test.log -d test.db3 -c control3.ctl -p proxies.txt
+python bibliometer.py -l logbook_test.log -d test.db3 -c control4.ctl -p proxies.txt
+python bibliometer.py -l logbook_test.log -d test.db3 -c control2.ctl -p proxies.txt
+python bibliometer.py -l logbook_test.log -d test.db3 -c control5.ctl -p proxies.txt
+python bibliometer.py -l logbook_test.log -d test.db3 -c control6.ctl -p proxies.txt
